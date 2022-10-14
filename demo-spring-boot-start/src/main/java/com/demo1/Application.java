f3f15901f879e7eb31bc09ae9040c88826d7ec41
shawn <euanxiao@gmail.com>
2022-10-14 14:33:28 +0800

    SpringBoot 启动演示模块

---
+++ demo-spring-boot-start/src/main/java/com/demo1/Application.java
@@ -0,0 +1,16 @@
+package com.demo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Yuan
+ * @description Application
+ * @date 2022/10/14
+ */
+@SpringBootApplication
+public class Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class,args);
+	}
+}
